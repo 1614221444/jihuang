88df6c0142d4700660ad49cf0fe63f97f6a62ddd
wuyh <1614221444@qq.com>
2019-12-05 19:13:39 +0800

    Initial commit

---
+++ Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,31 @@
+return {
+  ["workshop-100010005"]={
+    configuration_options={
+      MOD_RESTART_ALLOW_KILL=true,
+      MOD_RESTART_ALLOW_RESTART=true,
+      MOD_RESTART_ALLOW_RESURRECT=true,
+      MOD_RESTART_CD_KILL=3,
+      MOD_RESTART_CD_RESTART=5,
+      MOD_RESTART_CD_RESURRECT=7,
+      MOD_RESTART_FORCE_DROP_MODE=1,
+      MOD_RESTART_IGNORING_ADMIN=true,
+      MOD_RESTART_TRIGGER_MODE=1,
+      MOD_RESTART_WELCOME_TIPS=true,
+      MOD_RESTART_WELCOME_TIPS_TIME=6 
+    },
+    enabled=true 
+  },
+  ["workshop-100010021"]={
+    configuration_options={ food_estimation=-1, food_order=0, food_style=0, show_food_units=-1 },
+    enabled=true 
+  },
+  ["workshop-100010022"]={
+    configuration_options={ dont_give_dubloons=2, give_dubloons=1, position=1, rec_dubloon=3, rec_gold=3 },
+    enabled=true 
+  },
+  ["workshop-100010058"]={
+    configuration_options={ DFV_Language="CN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-2199027653598512929"]={ configuration_options={  }, enabled=true } 
+}
